5526541d5536d86f483cf0083de7b4fffa992e34
Pedro Henrique de Azevedo Wong Rego <phenrique.wong@gmail.com>
2024-10-23 14:12:34 -0300

    Listas

---
+++ src/listas/ListaEncadeadaTeste.java
@@ -0,0 +1,14 @@
+package listas;
+
+public class ListaEncadeadaTeste {
+
+	public static void main(String[] args) {
+		
+		ListaEncadeada lista = new ListaEncadeada<Integer>();
+		lista.adiciona(1);
+		
+		System.out.println(lista);
+		System.out.println("Tamanho da lista = " + lista.getTamanho());
+	}
+
+}
