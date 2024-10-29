8fe59e3f0dba9f22d41b0fe824bafced915a84fa
Pedro Henrique de Azevedo Wong Rego <phenrique.wong@gmail.com>
2024-10-29 13:49:58 -0300

    iterando uma lista

--- src/listas/ListaEncadeada.java
+++ src/listas/ListaEncadeada.java
@@ -5,7 +5,8 @@ public class ListaEncadeada<T> {
 	private No<T> ultimo;
 	private No<T> inicio;
 	private int tamanho;
-
+	
+//	adicionando ao final da lista
 	public void adiciona(T elemento) {
 		No<T> celula = new No<T>(elemento);
 
@@ -27,7 +28,29 @@ public class ListaEncadeada<T> {
 
 	@Override
 	public String toString() {
-		return "ListaEncadeada [inicio=" + inicio + "]";
+		
+		if(this.tamanho == 0) {
+			return "[]";
+		}
+		
+		StringBuilder builder = new StringBuilder();
+		
+		No<T> atual = this.inicio;
+		
+		for(int i = 0; i < this.tamanho -1; i++) {
+			builder.append(atual.getElemento()).append(", ");
+			atual = atual.getProximo();
+		}
+		builder.append(atual.getElemento());
+		
+		/*
+		 * builder.append(atual.getElemento()).append(",");
+		 * 
+		 * while(atual.getProximo() != null) { atual = atual.getProximo();
+		 * builder.append(atual.getElemento()).append(","); }
+		 */
+		
+		return builder.toString();
 	}
 
 }
